c5880a8e2e0d77f8ee65e0927c6bea9d8b632382
Tora <tora@raum.network>
2022-03-11 22:41:37 +0530

    A Simple Code to  Post Live Tweets for A Particular Account on Discord
    A Simple Code to  Post Live Tweets for A Particular Account on Discord on the Multiple parameters such as Mentioning, Hashtag's and Usernames.

---
+++ TwitterBot.js
@@ -0,0 +1,101 @@
+const needle = require('needle');
+require('dotenv').config()
+const Discord = require('discord.js');
+
+const { Client, Intents } = require('discord.js');
+const { json } = require('stream/consumers');
+const { getEventListeners } = require('events');
+
+const client = new Client({ intents: [Intents.FLAGS.GUILDS, Intents.FLAGS.GUILD_MESSAGES] });
+
+const token = process.env.BEARER_TOKEN;
+
+const rulesURL = 'https://api.twitter.com/2/tweets/search/stream/rules';
+const streamURL = 'https://api.twitter.com/2/tweets/search/stream';
+
+
+
+// Edit rules as desired below
+const rules = [{
+    'value': '@RaumNetwork',
+    'tag': 'TaggedAccount'
+},{
+    'value': '#RaumNetwork',
+    'tag': 'HashTaggedAccount'
+}];
+
+async function getAllRules() {
+
+    const response = await needle('get', rulesURL, {
+        headers: {
+            "authorization": `Bearer ${token}`
+        }
+    })
+
+    if (response.statusCode !== 200) {
+        console.log("Error:", response.statusMessage, response.statusCode)
+        throw new Error(response.body);
+    }
+
+    return(response.body);
+}
+
+
+function streamConnect(retryAttempt) {
+
+    const stream = needle.get(streamURL, {
+        headers: {
+            "User-Agent": "v2FilterStreamJS",
+            "Authorization": `Bearer ${token}`
+        },
+        timeout: 20000
+    });
+
+    stream.on('data', data => {
+        try {
+            const twitdata = JSON.parse(data);
+            const iddata = Object.values(twitdata.data)
+            console.log(iddata[0])
+            client.login(process.env.DISCORD_TOKEN);
+            client.once('ready', ()=> {
+                console.log(`the client becomes ready to start`);
+                //console.log(streamConnect.twitdata.data.id)
+                var url = "https://twitter.com/" + "RaumNetwork" + "/status/" + iddata[0]
+                console.log(url)
+                    try {
+                        let channel = client.channels.fetch(process.env.DISCORD_CHANNEL_ID).then(channel => {
+                            channel.send(url)
+                            }).catch(err => {
+                              console.log(err)
+                            })
+                        } catch (error) {
+                                console.error(error);
+                          }
+                        })
+            retryAttempt = 0;
+            
+        } catch (e) {
+            if (data.detail === "This stream is currently at the maximum allowed connection limit.") {
+                console.log(data.detail)
+                process.exit(1)
+            } else {
+            }
+        }
+    }).on('err', error => {
+        if (error.code !== 'ECONNRESET') {
+            console.log(error.code);
+            process.exit(1);
+        } else {
+            setTimeout(() => {
+                console.warn("A connection error occurred. Reconnecting...")
+                streamConnect(++retryAttempt);
+            }, 2 ** retryAttempt)
+        }
+    });
+
+    return stream  ;
+}
+
+streamConnect(0);
+
+
